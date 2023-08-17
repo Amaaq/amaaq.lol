28d927037effc2de42e29517aacbd438089b52ee
Amaaq <maaqoul.adil@gmail.com>
2023-08-17 15:17:02 +0100

    add models directory

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const {Schema, model} = require("mongoose")
+const userSchema = new Schema({
+    email: {
+        type : String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type : String,
+        required: true
+    },
+    verified: {
+        type : Boolean,
+        default: false
+    },
+    projects : {
+        type: String,
+        default : ""
+    },
+    refreshtoken: {
+        type: String
+    }
+})
+module.exports = model("User", userSchema)
