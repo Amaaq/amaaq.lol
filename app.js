58ec5be708030746b64e1811fd381c410404ae24
Amaaq <maaqoul.adil@gmail.com>
2023-08-18 21:58:44 +0100

    test

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ mongoose
   .then(()=>{
     console.log("MongoDB connection is established successfully!")
   })
+app.use(express.static(path.join(__dirname, '/')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
