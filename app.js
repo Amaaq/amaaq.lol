ae00b16b141ab874a4c536497ca0347c0492fdd4
Amaaq <maaqoul.adil@gmail.com>
2023-08-18 21:54:25 +0100

    test

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const PORT = 3000;
 
 const app = express();
 
+const mongoose = require("mongoose")
+mongoose 
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+  })
+  .then(()=>{
+    console.log("MongoDB connection is established successfully!")
+  })
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -21,13 +30,4 @@ app.listen(PORT, function () {
   console.log(`🚀 Listening on port ${PORT}`);
 });
 
-const mongoose = require("mongoose")
-mongoose 
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-  })
-  .then(()=>{
-    console.log("MongoDB connection is established successfully!")
-  })
 
