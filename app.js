79359f9e0d0b08c1c9b66bf563b8b98f7aa10434
Amaaq <maaqoul.adil@gmail.com>
2023-08-20 14:03:57 +0100

    view test

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
+app.use("/dist", indexRouter);
 app.use("/auth", authRouter);
 
 app.listen(PORT, function () {
