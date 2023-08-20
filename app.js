ec5c1b6bf26d41e182d489f08734f95ce098a33e
Amaaq <maaqoul.adil@gmail.com>
2023-08-20 14:00:07 +0100

    view test

--- app.js
+++ app.js
@@ -6,20 +6,12 @@ const path = require('path');
 
 const indexRouter = require("./routes/index");
 const authRouter = require("./routes/auth");
+const mongoose = require("mongoose")
 
 const PORT = 3000;
 
 const app = express();
 
-const mongoose = require("mongoose")
-mongoose 
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-  })
-  .then(()=>{
-    console.log("MongoDB connection is established successfully!")
-  })
 app.use(express.static(path.join(__dirname,'/dist')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,5 +23,13 @@ app.use("/auth", authRouter);
 app.listen(PORT, function () {
   console.log(`🚀 Listening on port ${PORT}`);
 });
+mongoose 
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+  })
+  .then(()=>{
+    console.log("MongoDB connection is established successfully!")
+  })
 
 
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res) {
-  res.sendFile('/index.html');
-});
+// router.get("/", function (req, res) {
+//   res.sendFile('/index.html');
+// });
 // router.get("/style.css", function (req, res) {
 //   res.sendFile(path.join(__dirname,'../','dist','/style.css'));
 // });
