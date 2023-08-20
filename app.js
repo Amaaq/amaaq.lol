a615cd6873866d1bedd8565cfad5fdd4c620add6
Amaaq <maaqoul.adil@gmail.com>
2023-08-20 13:56:50 +0100

    view test

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ mongoose
   .then(()=>{
     console.log("MongoDB connection is established successfully!")
   })
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname,'/dist')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
