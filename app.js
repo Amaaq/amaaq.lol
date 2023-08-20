a57d6b5d8d70062239fe15e35ba59d6aad5e6d11
Amaaq <maaqoul.adil@gmail.com>
2023-08-20 14:47:52 +0100

    view test

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const PORT = 3000;
 const app = express();
 
 app.use(express.static('dist/'))
+app.use('/assets', express.static(path.join(__dirname, "/dist/assets")))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
