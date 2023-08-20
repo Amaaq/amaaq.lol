b0040a949864569323a99718a771b7729eada01e
Amaaq <maaqoul.adil@gmail.com>
2023-08-20 14:31:14 +0100

    view test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
+var path = require('path')
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  res.sendFile(__dirname.slice(0,-7)+'/dist/index.html');
+  res.sendFile(path.join(__dirname,'../','/dist/index.html'));
 });
 // router.get("/style.css", function (req, res) {
 //   res.sendFile(path.join(__dirname,'../','dist','/style.css'));
