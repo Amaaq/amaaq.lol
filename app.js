fabc8ad478806bb083102748bd3d66fabe08c0ae
Amaaq <maaqoul.adil@gmail.com>
2023-08-20 14:02:41 +0100

    view test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,'/dist')));
+app.use(express.static('/dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
