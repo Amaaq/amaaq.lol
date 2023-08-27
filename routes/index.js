0885182044eb3655868590ecf9251044f13b1ce1
Amaaq <maaqoul.adil@gmail.com>
2023-08-27 15:39:49 +0100

    protected route testing

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var router = express.Router();
 var path = require('path')
+const { protected } = require("../utils/protected");
 
 /* GET home page. */
 router.get("/", function (req, res) {
   res.sendFile(__dirname.slice(0,-7)+'/dist/index.html');
 });
-router.get("/todo", function (req, res) {
+router.get("/todo",protected, function (req, res) {
   res.sendFile(__dirname.slice(0,-7)+'/dist/todo.html');
 });
 // router.get("/style.css", function (req, res) {
